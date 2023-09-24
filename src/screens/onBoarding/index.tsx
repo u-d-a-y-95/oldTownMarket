6bca48905616db6b616d393404f95da0d5e85dd0
UDay <uday.dev.bd@gmail.com>
2023-09-24 16:50:39 +0600

    design onboarding page

--- src/screens/onBoarding/index.tsx
+++ src/screens/onBoarding/index.tsx
@@ -4,9 +4,22 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
 } from "react-native";
+import { COLOR } from "../../utils/const";
+import { GlobalStyle } from "../../styles/global.style";
+import { NavigationProp } from "@react-navigation/native";
 const imageSrc = require("../../../assets/images/onboardBgImg.png");
-export default function OnBoarding() {
+
+type onBoardingProps = {
+  navigation: NavigationProp<{}>;
+};
+
+export default function OnBoarding({ navigation }: onBoardingProps) {
+  const getStartedButtonPressed = () => {
+    navigation.navigate("login");
+  };
+
   return (
     <View style={styles.root}>
       <ImageBackground
@@ -14,14 +27,46 @@ export default function OnBoarding() {
         style={styles.bgImg}
         resizeMode="cover"
       >
-        <Text>Welcome</Text>
-        <Text>to our store</Text>
-        <Text>Ger your groceries in as fast as one hour</Text>
-        <Pressable>
-          <View>
-            <Text>Get Started</Text>
-          </View>
-        </Pressable>
+        <View style={styles.body}>
+          <Image
+            source={require("../../../assets/splash.png")}
+            style={{ alignSelf: "center" }}
+          />
+          <Text
+            style={[
+              GlobalStyle.font_white,
+              GlobalStyle.font_2xl,
+              GlobalStyle.font_bold,
+            ]}
+          >
+            Welcome
+          </Text>
+          <Text
+            style={[
+              GlobalStyle.font_white,
+              GlobalStyle.font_2xl,
+              GlobalStyle.font_bold,
+            ]}
+          >
+            to our store
+          </Text>
+          <Text style={[GlobalStyle.font_white, GlobalStyle.font_md]}>
+            Ger your groceries in as fast as one hour
+          </Text>
+        </View>
+        <View style={styles.footer}>
+          <Pressable style={styles.button} onPress={getStartedButtonPressed}>
+            <Text
+              style={[
+                GlobalStyle.font_bold,
+                GlobalStyle.font_md,
+                GlobalStyle.font_white,
+              ]}
+            >
+              Get Started
+            </Text>
+          </Pressable>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -36,7 +81,25 @@ const styles = StyleSheet.create({
   bgImg: {
     width: "100%",
     height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
+    marginBottom: 40,
+    gap: 10,
+  },
+  footer: {
+    paddingBottom: 100,
+    width: "100%",
+    padding: 20,
+  },
+  button: {
+    backgroundColor: COLOR.PRIMARY,
+    alignItems: "center",
+    paddingVertical: 20,
+    borderRadius: 10,
   },
 });
---
+++ src/styles/global.style.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const GlobalStyle = StyleSheet.create({
+  font_sm: {
+    fontSize: 12,
+  },
+  font_md: {
+    fontSize: 16,
+  },
+  font_lg: {
+    fontSize: 20,
+  },
+  font_xl: {
+    fontSize: 24,
+  },
+  font_2xl: {
+    fontSize: 28,
+  },
+  font_bold: {
+    fontWeight: "bold",
+  },
+  font_white: {
+    color: "#fff",
+  },
+});
